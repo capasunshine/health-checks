e023ec2a1bd1c4bdbf60007ecda76a3173d9ea74
Aglika <a.darakchieva@gmail.com>
2020-05-12 00:16:59 -0400

    Add CPU load check

--- all_checks.py
+++ all_checks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import shutil
+import psutil
 import sys
 import socket
 
@@ -13,7 +14,7 @@ def check_disk_full(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
-    # Calculate how many free gigabytes
+    # Calculate the percentage of free space
     gigabytes_free = du.free / 2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
         return True
@@ -22,6 +23,10 @@ def check_disk_full(disk, min_gb, min_percent):
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise."""
+    return psutil.cpu_percentage(1) > 75
 
 def check_no_network():
     """Return True if it fails to resolve Google's URL, False otherwise"""
@@ -33,9 +38,10 @@ def check_no_network():
 
 def main():
     checks=[
-        (check_reboot, "Pending Reboot"),
-        (check_root_full, "Root partition full"),
-        (check_no_network, "No working network")
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full."),
+        (check_no_network, "No working network."),
+        (check_cpu_constrained, "CPU load too high.")
     ]
     everything_ok= True
     for check, msg in checks:
@@ -44,5 +50,12 @@ def main():
             everything_ok= False
     if not everything_ok:
         sys.exit(1)
+
+    if check_root_full():
+        print("Root partition full.")
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
 
 main()
