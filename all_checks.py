5aa59b9a148d2033fc26883a376f341c79d3d341
Aglika <a.darakchieva@gmail.com>
2020-05-12 00:07:35 -0400

    Add a simple network connectivity check

--- all_checks.py
+++ all_checks.py
@@ -2,27 +2,47 @@
 import os
 import shutil
 import sys
+import socket
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
-def check_disk_usage(disk, min_absolute, min_percent):
-    """This will return True if there is enough free disk space, False otherwise."""
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+def check_no_network():
+    """Return True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
         return False
-    return True
+    except:
+        return True
 
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full"),
+        (check_no_network, "No working network")
+    ]
+    everything_ok= True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok= False
+    if not everything_ok:
         sys.exit(1)
-    print("Everything ok.")
-    sys.exit(0)
 
 main()
